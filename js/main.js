27bb977815315265f8794e78094ef4e0fe47c3a5
gil <gino.ladowitch@gmail.com>
2019-06-17 11:33:46 +0200

    animation #7 ok with advert video

--- js/main.js
+++ js/main.js
@@ -1,19 +1,4 @@
 const widget = document.querySelector('.widget');
-const count = document.querySelector('.count .text');
-
-
-let nb = 24;
-
-function updateCounter() {
-    if (nb > 10) {
-        nb--;
-        count.textContent = `55:${nb}`;
-    } else {
-        nb = 24;
-    }
-}
-
-let counter = setInterval(updateCounter, 1000);
 
 
 window.addEventListener('keydown', function() {
